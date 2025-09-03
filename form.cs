23c9182ca25e219adcc2a990e76e6e78893c6d2d
Insaf MIM <misterinsh@gmail.com>
2025-09-03 10:43:06 +0530

    Create form.cs

---
+++ form.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace StudentManagementSystem
+{
+    public partial class Form1 : Form
+    {
+
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        internal static extern void DwmSetWindowAttribute(IntPtr hwnd,
+                                                     DWMWINDOWATTRIBUTE attribute,
+                                                     ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+                                                     uint cbAttribute);
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+            DwmSetWindowAttribute(this.Handle, attribute, ref preference, sizeof(uint));
+
+        }
+    }
+}
+//make sure to star this and share and help those who in need of this
